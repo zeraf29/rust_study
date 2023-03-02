71cfb75e21e8beac093f4f0dbc523af7315474a5
zeraf29 <zeraf29@gmail.com>
2023-03-03 07:24:19 +0900

    guessing game done

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,28 +3,34 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-   println!("숫자를 맞혀봅시다!");
-   println!("Please enter the answer number what you think");
+    println!("숫자를 맞혀봅시다!");
 
-   let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-   println!("The number user has to answer: {}", secret_number);
+    //println!("The number user has to answer: {}", secret_number);
 
-   println!("Please enter what you think correct answer.");
+    loop {
 
-   let mut guess = String::new();
-   io::stdin().read_line(&mut guess)
-	.expect("입력한 값을 읽지 못했습니다. Cannot read a entered number");
+        println!("Please enter what you think correct answer.");
 
-   let guess: u32 = guess.trim().parse()
-	.expect("입력한 값이 올바른 숫자가 아닙니다.");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+		.expect("입력한 값을 읽지 못했습니다.");
 
-   println!("Entered number: {}", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-   match guess.cmp(&secret_number){
-      Ordering::Less => println!("Entered number is less than secret_number"),
-      Ordering::Greater => println!("Entered number is bigger than secret_number"),
-      Ordering::Equal => println!("정답!"),
-   }      
+        println!("Entered number: {}", guess);
 
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Entered number is less than secret_number"),
+      	    Ordering::Greater => println!("Entered number is bigger than secret_number"),
+      	    Ordering::Equal => {
+                println!("정답!");
+                break;
+            }
+        }    
+    }      
 }

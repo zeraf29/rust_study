f17f78e22d39d23c3185ef3ec89edf8490eab9f4
zeraf29 <zeraf29@gmail.com>
2023-03-10 07:01:50 +0900

    variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -33,4 +33,43 @@ fn main() {
 
 //    let guess: u32 = "42".parse().expect("숫자가 아닙니다!"); 
 //    // 다른 데이터 타입으로 변환 시, 타입 애노테이션 annotation 을 이용해 타입을 명시해야 함. 
+
+
+    //부동소수점 타입
+    // let var_x = 2.0;             //f64
+       
+    // let var_y: f32 = 3.0;        // f32
+
+
+    //덧셈 
+    let sum = 5 + 10;
+
+    //뺄셈 
+    let difference = 95.5 - 4.3;
+
+    //곱셈
+    let product = 4 * 30;
+
+    //나눗셈
+    let quotient = 56.7 / 32.2;
+
+    //나머지
+    let remainder = 43 % 5;
+
+
+
+    //Boolean
+    let t = true;
+
+    let f: bool = false; //Type annotation 사용
+
+
+    //Character 
+    // 문자열 리터럴은 큰따옴표, char 리터럴은 작은따옴표
+    // 형식 chartset 별 유니코드 값은 다를 수 있으므로, 실제 표시 값은 다를 수 있음
+
+    let c = 'z';
+    let z = 'z';
+    //let heart_eyed_cat = '';
+
 }
